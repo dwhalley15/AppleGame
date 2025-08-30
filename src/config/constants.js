14f14af1a4ae88c3b2d0e25467309146ce272490
dwhalley15 <one_winged@hotmail.com>
2025-08-30 17:07:21 +0100

    dev improved some UI elements

--- src/config/constants.js
+++ src/config/constants.js
@@ -2,3 +2,6 @@ export const GAME_SIZE = { width: 500, height: 500 };
 export const BASE_SPEED = 300;
 export const MAX_LIVES = 3;
 export const DIFFICULTY_INCREMENT = 50;
+export const TEXT_COLOUR = "#0D0D35";
+export const FONT_FAMILY = "Arial";
+export const FONT_SIZE = "25px";
--- src/managers/ScoreManager.js
+++ src/managers/ScoreManager.js
@@ -1,20 +1,18 @@
-import { MAX_LIVES } from "../config/constants";
+import { MAX_LIVES, TEXT_COLOUR, FONT_FAMILY, FONT_SIZE } from "../config/constants";
 
 export default class ScoreManager {
   constructor(scene) {
     this.scene = scene;
     this.score = 0;
     this.lives = MAX_LIVES;
+    this.textStyle = {
+      font: `${FONT_SIZE} ${FONT_FAMILY}`,
+      fill: TEXT_COLOUR,
+    };
 
-    this.scoreText = scene.add.text(scene.scale.width - 125, 10, "Score: 0", {
-      font: "25px Arial",
-      fill: "#000000",
-    });
+    this.scoreText = scene.add.text(scene.scale.width - 10, 10, "Score: 0", this.textStyle).setOrigin(1, 0);
 
-    this.livesText = scene.add.text(10, 10, "Lives: 3", {
-      font: "25px Arial",
-      fill: "#000000",
-    });
+    this.livesText = scene.add.text(10, 10, "Lives: 3", this.textStyle);
   }
 
   addPoint() {
